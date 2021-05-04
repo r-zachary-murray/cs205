a02ccb73133647be6ab6b78401e0d602203047ab
YuxinYeIris <yye@g.harvard.edu>
2021-05-04 14:49:20 +0800

    add config.mk

---
+++ config.mk
@@ -0,0 +1,14 @@
+# Compiler and compilation flags
+cc=gcc
+cxx=g++
+cflags=-Wall -ansi -pedantic -O3 -pg
+
+# MPI compiler
+mpicxx=mpicxx -Wno-long-long
+
+# Flags for linking to PNG library
+png_iflags=-DHAS_PNG
+png_lflags=-lpng
+
+# LAPACK flags for dense linear algebra
+lp_lflags=-llapack -lblas
